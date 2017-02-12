416cab3850308563fa441871acef976309366b26
Norman Khine <norman@khine.net>
2017-02-12 15:27:21 +0000

    add Auth0 client id

--- src/_config.js
+++ src/_config.js
@@ -9,7 +9,7 @@ module.exports = function siteConfig(isProduction) {
       TWITTER: process.env.TWITTER || 'zmgc',
       GITHUB: process.env.GITHUB || 'TZM',
       GOOGLE_ANALYTICS_UA: (isProduction) ? 'UA-79510184-1' : 'dev',
-      AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID || 'oXN9xzJCJAU5BJxftjS4DbGN5hNhGnhk',
+      AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID || 'BSfx1Zvc3qsVXdAfe6MSaPuh0WAFKrAb',
       AUTH0_DOMAIN: process.env.AUTH0_DOMAIN || 'zmgc.auth0.com',
       ALGOLIA_ADMIN: process.env.ALGOLIA_ADMIN || '07aa17b67ca0a2609b9dd4b03d939a0a',
       SEGMENT_KEY: process.env.SEGMENT_KEY || 'gNftvcGllpKZTe5PcAxF8pEoenLq32H0',
